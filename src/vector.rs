da060175f37205d6e27055ddd4edea65a79e9ba6
Abrahan <scr44gr@gmail.com>
2023-06-11 01:06:31 -0400

    feat: add vector math functions

--- src/vector.rs
+++ src/vector.rs
@@ -20,6 +20,31 @@ impl Vec2 {
             y: self.y - other.y,
         }
     }
+
+    pub fn add(&self, other: Vec2) -> Vec2 {
+        Vec2 {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+
+    pub fn mul(&self, scalar: f32) -> Vec2 {
+        Vec2 {
+            x: self.x * scalar,
+            y: self.y * scalar,
+        }
+    }
+
+    pub fn div(&self, scalar: f32) -> Vec2 {
+        Vec2 {
+            x: self.x / scalar,
+            y: self.y / scalar,
+        }
+    }
+
+    pub fn len(&self) -> f32 {
+        self.dot(*self).sqrt()
+    }
 }
 
 #[derive(Debug, Copy, Clone)]
@@ -29,6 +54,7 @@ pub struct Vec3 {
     pub z: f32,
 }
 
+#[allow(dead_code)]
 impl Vec3 {
     pub fn new(x: f32, y: f32, z: f32) -> Vec3 {
         Vec3 { x, y, z }
@@ -51,4 +77,57 @@ impl Vec3 {
         let y = self.x * angle.sin() + self.y * angle.cos();
         Vec3 { x, y, z: self.z }
     }
+
+    pub fn dot(&self, other: Vec3) -> f32 {
+        self.x * other.x + self.y * other.y + self.z * other.z
+    }
+
+    pub fn sub(&self, other: Vec3) -> Vec3 {
+        Vec3 {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+        }
+    }
+
+    pub fn add(&self, other: Vec3) -> Vec3 {
+        Vec3 {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
+    }
+
+    pub fn mul(&self, scalar: f32) -> Vec3 {
+        Vec3 {
+            x: self.x * scalar,
+            y: self.y * scalar,
+            z: self.z * scalar,
+        }
+    }
+
+    pub fn div(&self, scalar: f32) -> Vec3 {
+        Vec3 {
+            x: self.x / scalar,
+            y: self.y / scalar,
+            z: self.z / scalar,
+        }
+    }
+
+    pub fn len(&self) -> f32 {
+        self.dot(*self).sqrt()
+    }
+
+    pub fn normalize(&self) -> Vec3 {
+        self.div(self.len())
+    }
+
+    pub fn cross(&self, other: Vec3) -> Vec3 {
+        Vec3 {
+            x: self.y * other.z - self.z * other.y,
+            y: self.z * other.x - self.x * other.z,
+            z: self.x * other.y - self.y * other.x,
+        }
+    }
+
 }
